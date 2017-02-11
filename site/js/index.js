b468549863dba2af1e2949b0d97c8580efa6db62
Michael Ho <michael.j.ho@gmail.com>
2017-02-11 11:42:35 -0800

    make gmaps non interactive until click

--- site/js/index.js
+++ site/js/index.js
@@ -1,5 +1,12 @@
-console.log('starting...');
 $('#clock').FlipClock(Math.round((new Date('2017-06-26').getTime() - new Date().getTime()) / 1000), {
     clockFace: 'DailyCounter',
     countdown: true
+});
+
+$('.gmaps-overlay').click(() => {
+    $('.gmaps-overlay iframe').css("pointer-events", "auto");
+});
+
+$('.gmaps-overlay').mouseleave(() => {
+    $('.gmaps-overlay iframe').css("pointer-events", "none");
 });
