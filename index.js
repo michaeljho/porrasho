61175a7d1251824ea48e3847952d0f75cb0a8f8b
Michael Ho <michael.j.ho@gmail.com>
2017-02-12 06:29:40 -0800

    setup mongoose, make forms work, add more content

--- index.js
+++ index.js
@@ -3,14 +3,63 @@ const logger = require('morgan');
 const favicon = require('serve-favicon');
 const compression = require('compression');
 const sass = require('node-sass-middleware');
+const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/porrasho');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('mongodb connected');
+});
+
+const showerRsvpSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    guest_count: Number
+});
+const ShowerRsvp = mongoose.model('ShowerRsvp', showerRsvpSchema);
+
+const nameVoteSchema = mongoose.Schema({
+    name: String,
+    other_name: String
+});
+const NameVote = mongoose.model('NameVote', nameVoteSchema);
 
 const app = express();
-const server = app.listen(80, '0.0.0.0');
+const server = app.listen(30000, '0.0.0.0');
 
 app.disable('x-powered-by');
 //app.use(favicon(__dirname + 'site/images/favicon.ico'));
 app.use(compression());
+
+app.use(bodyParser.urlencoded({
+    extended: false,
+    limit: '2mb'
+}));
+
+app.use(bodyParser.json({
+    limit: '2mb'
+}));
+
+app.post('/rsvp', (req, res) => {
+    console.log('received rsvp request', req.body);
+    const rsvp = new ShowerRsvp(req.body);
+    rsvp.save((err, rsvp) => {
+        if (err) return console.error(err);
+        res.send(rsvp);
+    });
+});
+
+app.post('/names', (req, res) => {
+    console.log('received name vote', req.body);
+    const name = new NameVote(req.body);
+    name.save((err, name) => {
+        if (err) return console.error(err);
+        res.send(name);
+    });
+});
 
 app.use('/styles', sass({
     src: path.join(__dirname, 'site/scss'),
--- site/js/index.js
+++ site/js/index.js
@@ -10,3 +10,76 @@ $('.gmaps-overlay').click(() => {
 $('.gmaps-overlay').mouseleave(() => {
     $('.gmaps-overlay iframe').css("pointer-events", "none");
 });
+
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
+
+$('input[name="other_name"]').keypress(() => {
+    $('input[name="name"][value="other"]').prop('checked', true);
+});
+
+$('#rsvp-submit').click((e) => {
+    e.preventDefault();
+    const form = $('#shower-rsvp');
+    const data = getFormData(form);
+    let error = false;
+
+    if (!data.name.trim().length) {
+        error = 'please enter a valid name';
+    }
+
+    if (!data.email.trim().length) {
+        error = 'please enter a valid email address';
+    }
+
+    if (error) {
+        form.siblings('.error-box').html(error);
+        return;
+    } else {
+        form.siblings('.error-box').html('');
+    }
+
+    $.ajax('/rsvp', {
+        method: 'POST',
+        data: data,
+        success: () => {
+            form.find('button.btn').html('submitted!').addClass('disabled');
+        }
+    });
+});
+
+$('#names-submit').click((e) => {
+    e.preventDefault();
+    const form = $('#baby-names');
+    const data = getFormData(form);
+    const success = form.find('button.btn').siblings('.success-text');
+    let error = false;
+    success.html('');
+
+    if (!data.name) {
+        error = 'please pick a name';
+    }
+
+    if (error) {
+        form.siblings('.error-box').html(error);
+        return;
+    } else {
+        form.siblings('.error-box').html('');
+    }
+
+    $.ajax('/names', {
+        method: 'POST',
+        data: data,
+        success: () => {
+            success.html('got it! feel free to submit another!');
+        }
+    });
+});
