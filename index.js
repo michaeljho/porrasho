d4f78baccbb698bb9d46850b69cc3e03a170d289
Michael Ho <michael.j.ho@gmail.com>
2017-02-13 01:29:28 -0800

    add nodemailer and send emails on rsvp/name suggestion

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const sass = require('node-sass-middleware');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 mongoose.connect('mongodb://localhost:27017/porrasho');
 
 const db = mongoose.connection;
@@ -26,6 +27,22 @@ const nameVoteSchema = mongoose.Schema({
     other_name: String
 });
 const NameVote = mongoose.model('NameVote', nameVoteSchema);
+
+const gmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mike.cilla.ho@gmail.com',
+        pass: '3H213BWV029EE'
+    }
+});
+
+const mailTpl = {
+    from: '"Mike and Priscilla" <mike.cilla.ho@gmail.com>',
+    to: '',
+    subject: '',
+    text: '',
+    html: ''
+};
 
 const app = express();
 const server = app.listen(80, '0.0.0.0');
@@ -48,6 +65,33 @@ app.post('/rsvp', (req, res) => {
     const rsvp = new ShowerRsvp(req.body);
     rsvp.save((err, rsvp) => {
         if (err) return console.error(err);
+
+        gmail.sendMail(Object.assign({}, mailTpl, {
+            to: rsvp.email,
+            subject: 'Thank you for RSVPing to our baby shower!',
+            text: 'We look forward to seeing you on March 18th! Keep an eye out for more details on our website or in future emails.\n-M&P | porrasho.com',
+            html: `<div>Hi ${rsvp.name}!</div>
+                <div style="margin-top:10px;">We look forward to seeing you on March 18th!</div>
+                <div>Keep an eye out for more details on our website or in future emails.</div>
+                <div style="margin-top:10px;">Mike & Priscilla<br/><a href="porrasho.com" target="_blank">porrasho.com</a></div>`
+        }), (error, info) => {
+            if (error) return console.log(error);
+            console.log('RSVP email confirmation sent:', info.messageId, info.response);
+        });
+
+        gmail.sendMail(Object.assign({}, mailTpl, {
+            to: 'mike.cilla.ho@gmail.com, parents@ferrenburg.com',
+            subject: `Baby Shower RSVP: ${rsvp.name} - ${rsvp.guest_count} guest${rsvp.guest_count > 1 ? 's' : ''}`,
+            text: `We look forward to seeing you on March 18th! Keep an eye out for more details on our website or in future emails.\n-M&P | porrasho.com`,
+            html: `<div>RSVP received:</div>
+                <div>${rsvp.name}</div>
+                <div>${rsvp.email}</div>
+                <div>Guests: ${rsvp.guest_count}</div>`
+        }), (error, info) => {
+            if (error) return console.log(error);
+            console.log('RSVP email info sent:', info.messageId, info.response);
+        });
+
         res.send(rsvp);
     });
 });
@@ -57,6 +101,17 @@ app.post('/names', (req, res) => {
     const name = new NameVote(req.body);
     name.save((err, name) => {
         if (err) return console.error(err);
+
+        gmail.sendMail(Object.assign({}, mailTpl, {
+            to: 'mike.cilla.ho@gmail.com',
+            subject: `Baby name suggestion: ${name.name === 'other' ? name.other_name : name.name}`,
+            text: `${name.name === 'other' ? name.other_name : name.name}`,
+            html: `${name.name === 'other' ? name.other_name : name.name}`
+        }), (error, info) => {
+            if (error) return console.log(error);
+            console.log('Baby name email info sent:', info.messageId, info.response);
+        });
+
         res.send(name);
     });
 });
